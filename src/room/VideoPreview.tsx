3de9d498b016ca8ba12142e9ac231032fb3f2ac4
melogale <zachariabadgers@gmail.com>
2026-03-16 02:02:36 -0400

    format

--- src/room/VideoPreview.tsx
+++ src/room/VideoPreview.tsx
@@ -11,10 +11,7 @@ import { facingModeFromLocalTrack, type LocalVideoTrack } from "livekit-client";
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 
-import {
-  allowPipSetting,
-  useSetting,
-} from "../settings/settings";
+import { allowPipSetting, useSetting } from "../settings/settings";
 
 import { TileAvatar } from "../tile/TileAvatar";
 import styles from "./VideoPreview.module.css";
