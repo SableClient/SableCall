565ee50257a9a65ae894488d1fcf9581f957138b
melogale <zachariabadgers@gmail.com>
2026-03-16 02:15:58 -0400

    embarassingly small commit

--- src/room/VideoPreview.tsx
+++ src/room/VideoPreview.tsx
@@ -12,7 +12,6 @@ import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 
 import { allowPipSetting, useSetting } from "../settings/settings";
-
 import { TileAvatar } from "../tile/TileAvatar";
 import styles from "./VideoPreview.module.css";
 import { type EncryptionSystem } from "../e2ee/sharedKeyManagement";
